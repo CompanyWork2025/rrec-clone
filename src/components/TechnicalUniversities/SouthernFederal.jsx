b8702f7e7679c37f917cdbd5a78bdcff233f3bca
Deepak Kumar <officewebdeveloper2025@gmail.com>
2025-03-20 13:07:17 +0530

    new tech add

--- src/components/TechnicalUniversities/SouthernFederal.jsx
+++ src/components/TechnicalUniversities/SouthernFederal.jsx
@@ -20,38 +20,142 @@ const SouthernFederal = () => {
                         alt="university"
                         className="mx-auto w-10 h-10 lg:w-20 lg:h-20"
                     />
-                    <h1 className="text-xl lg:text-3xl font-semibold mt-4">
+                </div>
+                <div className="mx-auto p-6 bg-white text-justify rounded-lg">
+                    {/* Introduction */}
+                    <h2 className="text-2xl text-center font-bold mb-2 text-red-600">
                         Southern Federal University
-                    </h1>
-                    <h2 className="text-md text-red-600 lg:text-xl mt-4 uppercase">
-                        FAMOUS UNIVERSITY OF RADIO-ENGINEERING, PREPARATORY COURSE
                     </h2>
+                    <p className="text-center font-semibold text-red-600">
+                        Learn Russian in SFedU | Top Rank University of Russia | SFedU Famous Worldwide
+                    </p>
+                    <p className="text-md mt-6">
+                        Southern Federal University abbreviated as SFedU and formerly known as Rostov State University (1957–2006), is a public university in Rostov Oblast, Russia with campuses in Rostov-on-Don and Taganrog. Southern Federal University is the largest research and educational establishment of Rostov Oblast. SFedU traces its history back to 1915, currently, there are ten Federal Universities in Russia, Southern federal university embraces four former higher educational establishments of the Rostov Region: Rostov State University in 1915 the year Imperial Warsaw University was transferred to the city of Rostov, Rostov State Teachers Training  University formed in 1930 out of the teachers training faculty within North Caucasian University, Taganrog State Radio Engineering University founded in 1952, and Rostov State Academy of Architecture and Art formed in 1988 on the basis of the Faculty of Architecture within Rostov Civil Engineering Institute.
+                    </p>
+                    <p className="text-md mt-6">
+                        SFedU occupied deserved the top place in the academic rankings of Russian Universities. The well-established traditions maintained by SFedU in the area of education and research; the image is positive and looks attractive to the prospective applicant. SfedU having the leading position in the area of innovations and research; integration of research into education, a winning combination of fundamental and applied research; highly reputed schools of thought; open-minded and creative teaching staff; up-to-date research facilities and appliances; collaboration with Russian Academy of sciences and international research facilities. SfedU best among all.
+                    </p>
 
-                    <div className="mt-4 space-y-6">
-                        <p className="text-left text-justify text-md lg:text-lg font-roboto">WELCOME TO SOUTHERN FEDERAL UNIVERSITY – A DYNAMIC, MODERN UNIVERSITY WITH HIGH STANDARDS IN TEACHING AND RESEARCH AND A GROWING REPUTATION FOR ENTERPRISE, EMPLOYMENT AND OPPORTUNITY. AT PRESENT SFEDU, THE LEGAL SUCCESSOR OF ROSTOV STATE UNIVERSITY, COMPRISES TECHNOLOGICAL, PEDAGOGICAL AND ARCHITECTURE INSTITUTES, 36 FACULTIES, 23 BRANCHES, 70 RESEARCH UNITS. SFEDU HAS 2 CAMPUSES IN ROSTOV AND TAGANROG. </p>
-                        <p className="text-left text-justify text-md lg:text-lg font-roboto">We take pride in the highly qualified and experienced university team: the academic and research staff includes 4 Full members of the Russian Academy of Sciences, 2 Associates of the Russian Academy of Sciences, 4 Associates of the Russian Academy of Education, 400 Professors and Doctors of Science, 1525 Candidates, 26 State Prizewinners and 17 Honorary Scientists of RF. Many SFedU alumni have become outstanding figures in their respective fields and include the Nobel Prize Winner – Alexander Solzhenitsyn, Full Members of the Academy of Science of RF, heads of state bodies, industry and research organizations, remarkable researchers and academics of foreign universities. SFedU has well-established international links with more than 300 universities and institutes from 50 countries. Our international outlook is strongly expressed in active collaboration with international foundations and programs, such as DAAD, CRDF, TACIS, INTAS, IREX, etc.</p>
-                        <p className="text-left text-justify text-md lg:text-lg font-roboto">The Associate membership at ECIU has given a new impulse to enhancing both internationalization and innovation spirit at SFedU. For more than 50 years the University provides training for international students and specialists. During that period 5007 persons from overseas completed their studies on undergraduate, graduate, professional development programs, and courses at the University. With a wide choice of courses across different programs, we invite you to join us at Southern Federal University and to learn and live as members of the university community.</p>
-                    </div>
-                    <h2 className="text-md text-red-600 lg:text-xl mt-4 uppercase">
-                        Faculties:
+                    {/* University Faculties */}
+                    <h2 className="text-xl text-red-600 font-bold mt-6 mb-2">
+                        Southern Federal University Faculties:
                     </h2>
-                    <p className="text-left text-justify text-md lg:text-lg font-roboto mt-4">Faculty of Architecture, Faculty of Arts, Faculty of Arts and Graphics, Faculty of Automation and Computer Science Faculty of Basic Preparation, Faculty of Biology and Soils, Faculty of Chemistry, Faculty of Economic and Social Systems Management, Faculty of Economics, Faculty of Economics, Management and Law, Faculty of Electronics and Electronic Equipment Engineering, Faculty of Geology and Geography, Faculty of High Technologies, Faculty of History, Faculty of Information Security, Faculty of Law Faculty of Linguistics, Faculty of Mathematics, Computer Science and Physics, Faculty of Mathematics, Mechanics & Computer Science, Faculty of Natural Science, Faculty of Natural Science and Humanities, Faculty of Pedagogics & Practical Psychology, Faculty of Philosophy and Culture Studies, Faculty of Philology and Journalism, Faculty of Physics, Faculty of Physical Culture & Sports, Faculty of Professional Development and Retraining of Teaching Staff, Faculty of Psychology, Faculty of Radio Engineering, Faculty of Regional Studies, Faculty of Social and Historical Education, Faculty of Social and Political Science, Faculty of Technology and Enterprise.</p>
+                    <ul className="list-disc pl-5">
+                        <li>Faculty of Biology and Soil Science</li>
+                        <li>Faculty of Chemistry</li>
+                        <li>Faculty of Economics</li>
+                        <li>Faculty of Geology & Geography</li>
+                        <li>Faculty of Preparatory Course (Learn Russian in Rostov – SFedU)</li>
+                        <li>Faculty of History</li>
+                        <li>Faculty of Law</li>
+                        <li>Faculty of Mathematics, Mechanics & Computer Science</li>
+                        <li>Faculty of Philology & Journalism</li>
+                        <li>Faculty of Philosophy & Cultural Studies</li>
+                        <li>Faculty of Psychology</li>
+                        <li>Faculty of Physics</li>
+                        <li>Faculty of Regional Studies</li>
+                        <li>Faculty of Social and Political Science</li>
+                    </ul>
+
+
+                    {/* Recognition and Ranking */}
+                    <h2 className="text-xl text-red-600 font-bold mt-6 mb-2">
+                        Institute of Economics and International Relations
+                    </h2>
+                    <h2 className="text-xl text-blue-600 font-bold mt-3 mb-2">
+                        Pedagogical Institute (former Rostov State Pedagogical University)
+                    </h2>
+                    <ul className="list-disc pl-5">
+                        <li>Faculty of Linguistics & Philology</li>
+                        <li>Faculty of Pedagogy & Psychology</li>
+                        <li>Faculty of Physical Education & Sport</li>
+                        <li>Faculty of Economics, Management & Legal Studies</li>
+                        <li>Faculty of Natural Sciences</li>
+                        <li>Faculty of Technology & Entrepreneurship</li>
+                        <li>Faculty of Visual Arts</li>
+                        <li>Faculty of Mathematics, Physics & Computer Sciences</li>
+                        <li>Faculty of Continuing and Extended Education</li>
+                        <li>Faculty of Advanced Training & Professional Development for Teachers</li>
+                    </ul>
+
+
+                    <div>
+                        <h2 className="text-xl text-red-600 font-bold mt-6 mb-2">Taganrog Institute of Technology (former Taganrog State University of Radio-engineering)</h2>
+                        <p>(Best University for Radio-Engineering in Russia)</p>
+
+                        <h2 className="text-xl text-red-600 font-bold mt-6 mb-2">Institute of Architecture & Arts (former Rostov State Academy of Architecture & Arts)</h2>
+
+                        <h2 className="text-xl text-red-600 font-bold mt-6 mb-2">Southern Federal University Recognition:</h2>
+                        <p>
+                            The Southern federal university is mainly recognized by the Ministry of Science and Higher Education of the Russian Federation. Sfedu having Memberships & association with International Association of Universities (IAU), Institutional Management in Higher Education (IMHE), European Association for International University (EAIE), European Consortium of Innovative Universities (ECIU)
+                        </p>
+
+                        <h2 className="text-xl text-red-600 font-bold mt-6 mb-2">Southern Federal University Ranking</h2>
+                        <p>
+                            Country Rank: 31, World Rank: 1522. SfedU is in big demand for a Preparatory course, Engineering courses, Law & other faculties available in university across the globe.
+                        </p>
+
+                        <h2 className="text-xl text-red-600 font-bold mt-6 mb-2">Southern Federal University Fees | Fee structure</h2>
+                        <p>
+                            The fee structure for 2024: 2800 USD Dollars for a Preparatory course with Tuition, Hostel, Medical Insurance, and for other courses 3200-4500 USD Dollars for the first year (It includes tuition, hostel, medical insurance, documentation). Fees mainly depend on the selected course of aspirant English & Russian faculties both available for foreign students, for confirmation consults RREC Team.
+                        </p>
+
+                        <h2 className="text-xl text-red-600 font-bold mt-6 mb-2">Southern Federal University Hostels | Accommodation</h2>
+                        <p>
+                            SfedU student’s hostels are fully new and with all facilities. The university hostels offer a variety of living facilities: sports campus, copy center, laundry, beauty salon, cafeteria, and other facilities are available here. Canteens are also available for daytime meals in All buildings of the University.
+                        </p>
+
+                        <h2 className="text-xl text-red-600 font-bold mt-6 mb-2">Southern Federal University Admission | Admission Procedure:</h2>
+                        <p>
+                            SfedU having one intake in a year for foreign students; Starts from May month to October every year. SfedU has a selective admission policy based on student previous education records and grades. International students are welcome to apply for enrollment.
+                        </p>
+
+                        <p>
+                            The admission process begins by you sending us the scanned copy of the documents listed below, for more details or free counseling consult RREC TEAM.
+                        </p>
+
+                        <ul className="list-disc pl-5">
+                            <li>Filled application form</li>
+                            <li>Passport</li>
+                            <li>Secondary school certificate or transcript</li>
+                            <li>Medical fitness certificate</li>
+                            <li>Photos (passport size 35*45mm at the matte paper)</li>
+                        </ul>
+
+                        <p className="mt-4">
+                            We will get you the admission letter and invitation letter from the selected university. We will also guide you through the entire process and will complete all documentation including visa, ticket (for Indian students). For other countries aspirants, we will provide admission and invitation letter and will provide complete guidelines regarding visas, tickets. The incoming student enrolled through RREC will receive by their officials at Moscow or at Rostov-on-Don airport.
+                        </p>
+
+                        <h2 className="text-xl text-red-600 font-bold mt-6 mb-2">Rostov-on-Don City</h2>
+                        <p>
+                            Rostov city founded in 1749, Rostov is the tenth most populous city in Russia. Rostov-on-Don is a big city in Southern Russia. The Rostov Regional Museum of Local History has collections on Cossack and war history. Nearby, Rostov Regional Museum of Fine Arts displays Russian art from the 16th-20th centuries. The Rostov Academic Drama Theater of Maxim Gorky, Built in the shape of the tractor, presents classic and contemporary plays. Outside, Theater Square has fountains and a Ferris wheel. Best weather across the year, that way Rostov-on-Don Universities always the first selection of foreign students. One of the hosted city of Fifa world cup.
+                        </p>
+                    </div>
+
                 </div>
             </div>
 
             <div className="bg-red-700 mt-2 mb-6 rounded-xl px-8 py-16 max-w-4xl lg:max-w-7xl mx-auto text-center font-roboto text-white">
                 <h2 className="text-xl lg:text-2xl mb-4 font-semibold">
-                    Study in Southern federal university
+                    Get Direct Admission to DSTU Join Reliable Russian Educational Consultants
                 </h2>
                 <h3 className="text-lg lg:text-2xl font-semibold mb-4">
-                    Admission open for this academic year, interesting students may contact us
+                    (Authorized official representative of Indians & foreign students)
                 </h3>
                 <h3 className="text-lg lg:text-2xl font-semibold mb-4">
-                    Contact numbers: +7-9515190376 (Rus) |+917428212236 (Ind)
+                    Admission open for the 2024 academic year, interesting students may contact us
                 </h3>
-                <h6 className="mb-4">Email: thedoctors.iea@gmail.com</h6>
-                <h6>Students or parents can write to us on at WhatsApp also +7-9515190376 (24*7)</h6>
+                <h3 className="text-lg lg:text-2xl font-semibold mb-4">
+                    Contact numbers: +7-9515371133 (Rus) | +917042284508, 509 (Ind)
+                </h3>
+                <h6 className="mb-4">
+                    Email: contact@rrecrussia.com, rrec.educonsultants@gmail.com
+                </h6>
+                <h6>
+                    Students or parents can write to us at WhatsApp also +7-9515371133 (24*7)
+                </h6>
             </div>
+
         </>
     )
 }
