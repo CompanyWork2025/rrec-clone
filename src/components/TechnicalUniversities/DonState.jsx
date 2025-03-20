a0350075b58f13a0d9489b704b83e026fc4006df
Deepak Kumar <officewebdeveloper2025@gmail.com>
2025-03-20 12:55:02 +0530

    tech updated

--- src/components/TechnicalUniversities/DonState.jsx
+++ src/components/TechnicalUniversities/DonState.jsx
@@ -21,70 +21,102 @@ const DonState = () => {
                         alt="university"
                         className="mx-auto w-10 h-10 lg:w-20 lg:h-20"
                     />
-                    <h1 className="text-xl lg:text-3xl font-semibold mt-4">
-                        Don State Technical University
-                    </h1>
-                    <h2 className="text-md text-red-600 lg:text-xl mt-4 uppercase">
-                        STUDY IN ROSTOV | DONSTU ROSTOV-ON-DON
-                    </h2>
-                    <h2 className="text-md text-red-600 lg:text-xl mt-4 uppercase">
-                        LEARN RUSSIAN LANGUAGE IN DSTU
-                    </h2>
-                    <div className="mt-4 space-y-6">
-                        <p className="text-left text-justify text-md lg:text-lg font-roboto">Don State Technical University dates back to 1930 when it was established as the North-Caucasus Institute of Agricultural Machine-Building. In 1932-1992 under the name of the Rostov Institute of Agricultural Machine Building, it was truly considered to be the leading educational and research technical institution of the USSR with 4 faculties and 25 specialties. 1992 turned the story up to university status and the name of Don State Technical University. Nowadays DSTU is known throughout the Russian Federation and internationally as an innovative educational and research complex providing education for children, youth, and adults according to lifelong learning principles.  It involves kindergartens, colleges, technical lyceum and gymnasium, higher educational institutions, business schools, and affiliations throughout Russia and abroad.   </p>
-                        <p className="text-left text-justify text-md lg:text-lg font-roboto">Being situated in Rostov-on-Don, the largest city in the south of the Russian Federation, a well-known scientific, educational, industrial, and cultural center, one of the hosts of the Football World Cup and other worldwide events, DSTU is famous as a unique platform for placing international events, forums and meetings. Seminars, symposia, and conferences are organized in DSTU Congress-hall to bring together people interested in science and technology, education, research, and development.</p>
-                    </div>
-                    <h2 className="text-md text-red-600 lg:text-xl mt-4 uppercase">
-                        DON STATE TECHNICAL UNIVERSITY
-                    </h2>
-                    <p className="text-left text-justify text-md lg:text-lg font-roboto mt-4">DSTU is participating actively in knowledge-based society creation according to the Bologna Declaration and lifelong learning principles implementation. Nowadays more than 50 000 students, including 1600 foreigners, and 200 post-graduates get an education at different institutions and faculties of the University. Students have training in the largest local companies: Rostov helicopter plant, Rostov combine-harvester plant, and others.</p>
-                    <h2 className="text-md text-red-600 lg:text-xl mt-8">
-                        FACULTIES & INSTITUTES OF DSTU:
-                    </h2>
-                    <p className="text-left text-justify text-md lg:text-lg font-roboto mt-4">Preparatory faculty (Russian Language course)</p>
-                    <p className="text-left text-justify text-md lg:text-lg font-roboto mt-4">Oil and Gas</p>
-                    <p className="text-left text-justify text-md lg:text-lg font-roboto mt-4">Life safety and environmental engineering</p>
-                    <p className="text-left text-justify text-md lg:text-lg font-roboto mt-4">Nanotechnologies and composite materials</p>
-                    <p className="text-left text-justify text-md lg:text-lg font-roboto mt-4">Innovative business and management</p>
-                    <p className="text-left text-justify text-md lg:text-lg font-roboto mt-4">Management and Entrepreneurship</p>
-                    <p className="text-left text-justify text-md lg:text-lg font-roboto mt-4">Agricultural machinery and equipment</p>
-                    <p className="text-left text-justify text-md lg:text-lg font-roboto mt-4">Agro engineering</p>
-                    <p className="text-left text-justify text-md lg:text-lg font-roboto mt-4">Mechanical engineering</p>
-                    <p className="text-left text-justify text-md lg:text-lg font-roboto mt-4">Tools and instrument engineering</p>
-                    <p className="text-left text-justify text-md lg:text-lg font-roboto mt-4">Aircraft construction, vehicle service </p>
-                    <p className="text-left text-justify text-md lg:text-lg font-roboto mt-4">Mechanical engineering technologies and machinery</p>
-                    <p className="text-left text-justify text-md lg:text-lg font-roboto mt-4">Electrical Mechanics and Production Machines</p>
-                    <p className="text-left text-justify text-md lg:text-lg font-roboto mt-4">Automation, mechatronics, and control</p>
-                    <p className="text-left text-justify text-md lg:text-lg font-roboto mt-4">Informatics and computer sciences</p>
-                    <p className="text-left text-justify text-md lg:text-lg font-roboto mt-4">Media communications and multimedia technologies</p>
-                    <p className="text-left text-justify text-md lg:text-lg font-roboto mt-4">The faculty of international students preparation</p>
-                    <p className="text-left text-justify text-md lg:text-lg font-roboto mt-4">Humanities and social sciences</p>
-                    <p className="text-left text-justify text-md lg:text-lg font-roboto mt-4">Azov Institute of Technology</p>
-                    <p className="text-left text-justify text-md lg:text-lg font-roboto mt-4">Taganrog Polytechnic Institute</p>
-                    <p className="text-left text-justify text-md lg:text-lg font-roboto mt-4">DSTU Branch in Volgodonsk</p>
-                    <p className="text-left text-justify text-md lg:text-lg font-roboto mt-4">Institute of Service and Technology in Pyatigorsk</p>
-                    <p className="text-left text-justify text-md lg:text-lg font-roboto mt-4">Technological Institute of Service in Stavropol</p>
-                    <p className="text-left text-justify text-md lg:text-lg font-roboto mt-4">Institute for the service and  entrepreneurship in Shakhty</p>
                 </div>
 
-                <h1 className="text-xl lg:text-2xl text-red-600 font-semibold text-center mt-8">
-                    “BEST TECHNICAL UNIVERSITY OF RUSSIA FOR FOREIGN STUDENTS: DON STATE TECHNICAL UNIVERSITY”
-                </h1>
-                <h1 className="text-xl lg:text-2xl text-red-600 font-semibold text-center mt-4">
-                    English Medium Courses are available for: Engineering, MBA, Veterinary Medicine. Fees start from 3000 USD Dollars. Hostels are in campus.
-                </h1>
-            </div>
+                <div className="mx-auto p-6 bg-white text-justify rounded-lg">
+                    {/* Introduction */}
+                    <h2 className="text-2xl text-center font-bold mb-2 text-red-600">Don State Technical University Russia</h2>
+                    <p className="text-center font-semibold text-red-600">Study in Russia Don State Technical University | Direct Admissions with RREC Team</p>
+                    <p className="text-md mt-6 ">DSTU dates back to 1930 when it was established as the North-Caucasus Institute of Agricultural Machine-Building. In 1932-1992 under the name of the Rostov Institute of Agricultural Machine Building, it was truly considered to be the leading educational and research technical institution of the USSR with 4 faculties and 25 specialties. 1992 turned the story up to university status and the name of Don State Technical University. Nowadays DSTU is known throughout the Russian Federation and internationally as an innovative educational and research complex providing education for children, youth, and adults according to lifelong learning principles. It involves kindergartens, colleges, technical lyceum and gymnasium, higher educational institutions, business schools, and affiliations throughout Russia and abroad. Being situated in Rostov-on-Don, the largest city in the south of the Russian Federation, a well-known scientific, educational, industrial, and cultural center, one of the hosts of the Football World Cup and other worldwide events, DSTU is famous as a unique platform for placing international events, forums and meetings. Seminars, symposia, and conferences are organized in DSTU Congress-hall to bring together people interested in science and technology, education, research, and development.</p>
+                    <p className="text-md mt-6 ">DonSTU is participating actively in knowledge-based society creation according to the Bologna Declaration and lifelong learning principles implementation. Nowadays more than 50 000 students, including 1600 foreigners, and 200 post-graduates get an education at different institutions and faculties of the University Students to have their in-company training in the largest local companies: Rostov helicopter plant, Rostov combine-harvester plant, and others.</p>
 
+
+                    {/* University Faculties */}
+                    <h2 className="text-xl text-red-600 font-bold mt-6 mb-2">
+                        Don State Technical University Faculties | Institutes of DSTU:
+                    </h2>
+                    <ul className="list-disc pl-5">
+                        <li>Preparatory Faculty (Russian Language Course)</li>
+                        <li>Oil and Gas</li>
+                        <li>Life Safety and Environmental Engineering</li>
+                        <li>Nanotechnologies and Composite Materials</li>
+                        <li>Innovative Business and Management</li>
+                        <li>Management and Entrepreneurship</li>
+                        <li>Agricultural Machinery and Equipment</li>
+                        <li>Agro Engineering</li>
+                        <li>Mechanical Engineering</li>
+                        <li>Tools and Instrument Engineering</li>
+                        <li>Aircraft Construction, Vehicle Service, and Operation</li>
+                        <li>Mechanical Engineering Technologies and Machinery</li>
+                        <li>Electrical Mechanics and Production Machines</li>
+                        <li>Automation, Mechatronics, and Control</li>
+                        <li>Informatics and Computer Sciences</li>
+                        <li>Media Communications and Multimedia Technologies</li>
+                        <li>Faculty of International Students Preparation</li>
+                        <li>Humanities and Social Sciences</li>
+                        <li>Azov Institute of Technology</li>
+                        <li>Taganrog Polytechnic Institute</li>
+                        <li>DSTU Branch in Volgodonsk</li>
+                        <li>Institute of Service and Technology in Pyatigorsk</li>
+                        <li>Technological Institute of Services in Stavropol</li>
+                        <li>Institute of the Service and Entrepreneurship in Shakhty</li>
+                    </ul>
+
+
+                    {/* Recognition and Ranking */}
+                    <h2 className="text-xl text-red-600 font-bold mt-6 mb-2">Don State Technical University Recognition:</h2>
+                    <p>The Don State Technical University is mainly recognized by the Ministry of Science and Higher Education of Russia, Membership of Association of engineering education of Russia (AEER), International Association for continuing engineering education (IACEE), Association of Sino-Russian Technical Universities (ASRTU).</p>
+
+
+                    {/* Fee Structure */}
+                    <h2 className="text-xl text-red-600 font-bold mt-6 mb-2">Don State Technical University Ranking</h2>
+                    <p>Country Rank: 61, World Rank: 2921. DSTU is the first choice for foreign students to study Engineering, Russian language Courses in Russia.</p>
+
+
+                    {/* Hostel and Accommodation */}
+                    <h2 className="text-xl text-red-600 font-bold mt-6 mb-2">Hostels & Accommodation</h2>
+                    <p>DSTU offers seven hostels with comfortable rooms, designed for 2-3 students per room. Living on campus allows students to engage in university life and meet fellow international students.</p>
+
+                    {/* Admission Procedure */}
+                    <h2 className="text-xl text-red-600 font-bold mt-6 mb-2">Don State Technical University Hostels | Accommodation</h2>
+                    <p>DSTU offers seven hostels with comfortable rooms with all the necessary facilities. Our dormitory is designed for the comfortable living of two or three students in a room. Living on the campus gives you the opportunity to immerse yourself in student life to meet other foreign students and make friends from all over the world.</p>
+                    <p>Don state technical university Admission | Admission Procedure: DSTU having one intake in a year for foreign students; Starts from June month to October 15 every year. The old higher university DSTU has a selective admission based on the entrance exam and student’s secondary school records & grades. International Students are welcome to apply for enrollme</p>
+                    <p>The admission process begins by you sending us the scanned copy of the documents listed below, for more details or free counseling consult RREC TEAM.</p>
+                    <ul className="list-disc pl-5">
+                        <li>Online Application Form</li>
+                        <li>Passport</li>
+                        <li>Secondary School Certificate / Transcript</li>
+                        <li>Medical Fitness Certificate</li>
+                        <li>Passport-size Photos (35x45mm, matte paper)</li>
+                    </ul>
+                    <p>We will get you the admission letter and invitation letter from the selected university. We will also guide you through the entire process and will complete all documentation including visa, ticket (for Indian students), For other countries aspirants, we will provide admission and invitation letter and will provide complete guidelines regarding visas, tickets. An incoming student enrolled through RREC will receive it by their officials at Moscow or Rostov-on-Don airport.</p>
+
+                    {/* Rostov-on-Don */}
+                    <h2 className="text-xl text-red-600 font-bold mt-6 mb-2">Rostov-on-Don</h2>
+                    <p>Rostov-on-Don is a port city and the administrative center of Rostov Oblast and the Southern Federal District of Russia. It lies to the southeast of the East European Plain, on the Don River, 32 kilometers from the Sea of Azov. The southwestern suburbs of the city about the Don River delta. The population of Rostov-on-Don is over one million people. One of the hosted cities of FIFA world cup. </p>
+                </div>
+            </div>
             <div className="bg-red-700 mt-2 mb-6 rounded-xl px-8 py-16 max-w-4xl lg:max-w-7xl mx-auto text-center font-roboto text-white">
                 <h2 className="text-xl lg:text-2xl mb-4 font-semibold">
-                    Call for more details about the university admission, processing, fees at DonSTU, Russia
+                    Get Direct Admission to DSTU – Join Reliable Russian Educational Consultants
                 </h2>
                 <h3 className="text-lg lg:text-2xl font-semibold mb-4">
-                    Contact numbers: +7-9515190376 (Rus) | +917428212236 (Ind)
+                    (Authorized Official Representative of Indian & Foreign Students)
                 </h3>
-                <h6 className="mb-4">Email: thedoctors.iea@gmail.com, contact@thedoctorsiea.in</h6>
-                <h6>Students or parents can write to us on at WhatsApp also +7-9515190376 (24*7)</h6>
+                <p className="text-base lg:text-lg mb-4">
+                    Admission open for the 2024 academic year. Interested students may contact us.
+                </p>
+                <h3 className="text-lg lg:text-2xl font-semibold mb-4">
+                    Contact Numbers: +7-9515371133 (Russia) | +91-7042284508, +91-7042284509 (India)
+                </h3>
+                <h6 className="mb-4">
+                    Email: contact@rrecrussia.com, rrec.educonsultants@gmail.com
+                </h6>
+                <h6>
+                    Students or parents can write to us on WhatsApp also: +7-9515371133 (24x7)
+                </h6>
             </div>
+
         </>
     )
 }
