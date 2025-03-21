d7536616fa60310c7cab6205ae907f7ca72a20bc
Deepak Kumar <officewebdeveloper2025@gmail.com>
2025-03-21 15:11:17 +0530

    volgo updated

--- src/components/MedicalUniversities/Volgograd.jsx
+++ src/components/MedicalUniversities/Volgograd.jsx
@@ -45,7 +45,7 @@ const Volgograd = () => {
                     <h1 className="text-xl lg:text-3xl font-semibold mt-4">
                         Volgograd State Medical University, Russia
                     </h1>
-                    <h2 className="text-md text-red-600 lg:text-xl mt-4">
+                    <h2 className="text-md text-red-600 font-semibold lg:text-xl mt-4">
                         Volgograd State Medical University – Know Everything About Admission, Fess, and Hostel Facility
                     </h2>
                     <div className="mt-4 space-y-6">
@@ -53,13 +53,13 @@ const Volgograd = () => {
                         <p className="text-left text-justify text-md lg:text-lg font-roboto">Currently, this university is headed by the prestigious and eminent Russian scientist, Doctor of Medical Science, Professor Shkarin Vladimir Vyacheslavovich. One can get all relevant information at Volgograd State Medical University's official website as well.</p>
                         <p className="text-left text-justify text-md lg:text-lg font-roboto">Complete your Medical Degree from World-Renowned University in Affordable Fees. Best Results in FMGE.</p>
                     </div>
-                    <h2 className="text-md text-red-600 lg:text-xl mt-8">
+                    <h2 className="text-md text-red-600 font-semibold lg:text-xl mt-8">
                         Know More About Volgograd State Medical University
                     </h2>
                     <p className="text-left text-justify text-md lg:text-lg font-roboto mt-4">Volgograd State Medical University, Russia, also abbreviated as VolgSMU or VSMU, is a very old medical institution in Russia. It is located in Volgograd which falls in the southern part of Russia on the bank of the river Volga. It is established in 1935 and got its status for the Academy in 1993. In the year 2003, it is granted the status of the University. VSMU is considered among the best 10 medical universities in Russia. As a student, if you want to know the high reputation of this university, you should know that in 1962, the university opened the door for international students. Presently more than 4500 medical experts or specialists are working in 127 countries of the world with VSMU degrees. It started MBBS in English in the year 2000.</p>
                     <p className="text-left text-justify text-md lg:text-lg font-roboto mt-4">Apart from its history and tradition of producing world-class future doctors, it is also known for its highly-qualified teaching staff, experienced physicians, and advanced researchers who are world-famous for their contribution in their domain. The number of staff in the university is 763. It encompasses highly qualified professors and instructors. The university has 96 professors, 11 academicians, 401 associate professors. Mostly they are equipped with Ph.D. or more advanced degrees.</p>
                     <p className="text-left text-justify text-md lg:text-lg font-roboto mt-4">Gateway for Admission in VSMU: Choose the MBBS consultant Russia carefully as the wrong selection can impact your career for the rest of the life. Apply for the VSMU MBBS course through RREC (Doctors-Ideal Educational Advisors) as we are the authorized and official representative for MBBS in VSMU, Russia. You can get personalized support, authentic information, and guidance until you get admission to VSMU. You can personally visit our office or have a telephonic conversation regarding any inquiry or information you need for admission to MBBS. We have successfully helped a large number of students in India pursuing their MBBS goal in Russia. Contact us to find the best medical university in Russia.</p>
-                    <h2 className="text-md text-red-600 lg:text-xl mt-8">
+                    <h2 className="text-md text-red-600 font-semibold lg:text-xl mt-8">
                         Recognition, Accreditation, and International Ties
                     </h2>
                     <p className="text-left text-justify text-md lg:text-lg font-roboto mt-4">VSMU is listed in the World Directory of Medical Schools. It is approved and recognized by WHO and NMC of India. It is one of the best universities in Russia in terms of creating and developing tie-ups with leading international Universities. The university is also accredited by FAIMER and has also been working as a collaborative effort with the WHO, British Council, the World Federation for Medical Education, the United Nations Education, Social and Cultural Organization, and the US Agency for International Development.</p>
@@ -69,7 +69,7 @@ const Volgograd = () => {
 
                 {/* Middle Section */}
                 <div className="text-center">
-                    <h2 className="text-md text-red-600 lg:text-xl mt-8 mb-2">
+                    <h2 className="text-md text-red-600 font-semibold lg:text-xl mt-8 mb-2">
                         University Summary at a Glance
                     </h2>
                     <div className="overflow-x-auto">
@@ -84,11 +84,11 @@ const Volgograd = () => {
                             </tbody>
                         </table>
                     </div>
-                    <h2 className="text-md text-red-600 lg:text-xl mt-8">
+                    <h2 className="text-md text-red-600 font-semibold lg:text-xl mt-8">
                         Duration of MBBS Program
                     </h2>
                     <p className="text-left text-justify text-md lg:text-lg font-roboto mt-2">The duration of the MBBS course at Volgograd State Medical University is 6 years. This is for those who are pursuing the English medium course, for the Russian Language course the duration for MBBS is 7 years. In addition to it, medical universities in Russia facilitate students to work during your span of 6 years so that they get real-time experience as well. </p>
-                    <h2 className="text-md text-red-600 lg:text-xl mt-8">
+                    <h2 className="text-md text-red-600 font-semibold lg:text-xl mt-8">
                         Why Doing MBBS at VSMU is an Excellent Choice for Indian Students?
                     </h2>
                     <p className="text-left text-justify text-md lg:text-lg font-roboto mt-2">Getting admission to government medical colleges or universities in India is an extremely difficult task. More than 16 lacs students appear for the medical entrance exam every year to qualify for mere 35000 seats. Moreover, the cost of MBBS in our country is not affordable for everyone. Under such circumstances, when universities like Volgograd State Medical University, Russia offer direct admission in MBBS that too in affordable cost, it becomes irresistible for the students to hold them back to pursue their dream career abroad. This is the reason why Volgograd State Medical University reviews are quite splendid over the years. Some top-notch factors to do MBBS from VSMU are given below:</p>
@@ -102,32 +102,32 @@ const Volgograd = () => {
                     <p className="text-left text-justify text-md lg:text-lg font-roboto mt-2">Extracurricular Activities and Physical Training: The University has a dedicated department that facilitates and promotes physical training. The department incorporates a sports ground gym, mini stadium, etc. Every year the cultural program is held by international students. Cricket and football tournaments are also organized by Indian and Malaysian students.</p>
                     <p className="text-left text-justify text-md lg:text-lg font-roboto mt-2">Faculty & Staff: Professors, teachers, staff, etc. have a significant role to play in every university. University has attracted highly qualified faculty and staff to nurture and train students to a global level. This ensures you get nothing less than optimum medical education. Departments, teachers, and staff all work in harmony to ensure proper education, safety, and facility of the students without any bias.</p>
                     <p className="text-left text-justify text-md lg:text-lg font-roboto mt-2">Worldwide Recognized Degree: The MBBS Degree obtained from VSMU is globally recognized. Government hospitals provide practical training to medical students in Russia. Students are eligible to work in a government hospital after completing their MBBS program.</p>
-                    <h2 className="text-md text-red-600 lg:text-xl mt-8">
+                    <h2 className="text-md text-red-600 font-semibold lg:text-xl mt-8">
                         Volgograd State Medical University Ranking
                     </h2>
                     <p className="text-left text-justify text-md lg:text-lg font-roboto mt-2">VSMU is considered among the top ten universities in Russia due to top-level medical education under an affordable budget. The university ranking is at par. Volgograd State Medical University Country Ranking Is 105, on the other Volgograd State Medical University Ranking on the international circuit (World ranking) is 3103, according to 4icu.org. The ranking reveals the overall quality, standard, and facility provided to the students studying medical education.</p>
-                    <h2 className="text-md text-red-600 lg:text-xl mt-8">
+                    <h2 className="text-md text-red-600 font-semibold lg:text-xl mt-8">
                         Clearing FMGE/NEXT License after MBBS Degree
                     </h2>
                     <p className="text-left text-justify text-md lg:text-lg font-roboto mt-2">Students completing MBBS degree from abroad will have to appear in the NEXT/FMGE exam in order to acquire eligibility to practice in India or getting registration in NMC. Previously, this exam was held by NBE (National Board of Exam). The point of exhilaration for students doing MBBS from VSMU is that the percentage of pass students in the NEXT/FMGE examination is high. The team of RREC will help you to find the best coaching module for FMGE preparation.</p>
-                    <h2 className="text-md text-red-600 lg:text-xl mt-8">
+                    <h2 className="text-md text-red-600 font-semibold lg:text-xl mt-8">
                         Volgograd City:
                     </h2>
                     <p className="text-left text-justify text-md lg:text-lg font-roboto mt-2">Volgograd, formerly Stalingrad, is a city in southwest Russia, on the western bank of the Volga River. It was the site of WWII's Battle of Stalingrad, commemorated by a huge statue, The Motherland Calls, part of the hilltop Mamayev Kurgan memorial complex. The Panorama Museum has a 360-degree painting of the battle, as well as weapons and artifacts. To the south, a large arch marks the Lenin Volga-Don Shipping Canal.
                         The international airport in the city; students can travel directly or through Moscow to Volgograd. </p>
-                    <h2 className="text-md text-red-600 lg:text-xl mt-8">
+                    <h2 className="text-md text-red-600 font-semibold lg:text-xl mt-8">
                         Why should You Choose Team RREC for Admission to VSMU
                     </h2>
                     <p className="text-left text-justify text-md lg:text-lg font-roboto mt-2">RREC team is specialized and expert in offering dedicated service and consultancy for admission in MBBS in Russia. After working and exploring this field for years, we can say that our service is best to none. Moreover, we are the official and authorized representative for admission to VSMU and many other universities in Russia. So far, we have been immaculate in providing seamless assistance and zero rejection record. More than 1200 students have already placed in Russian Universities and it's still counting. We gear you up with expert advice and help you make the right decision at the right time.</p>
                     <p className="text-left text-justify text-md lg:text-lg font-roboto mt-2">We offer personal counseling, tele-counseling, and online counseling. Drop-in personally or contact us by phone, you will find us helpful and appropriate for all your admission related need. Do not hesitate to get in touch with us we are always there to help you.</p>
-                    <h2 className="text-md text-red-600 lg:text-xl mt-8">
+                    <h2 className="text-md text-red-600 font-semibold lg:text-xl mt-8">
                         Some points to remember to avoid agents cheating
                     </h2>
                     <p className="text-left text-justify text-md lg:text-lg font-roboto mt-2">Always pay the fees directly into the account of the university. No need to pay any fees amount to sub-agents.</p>
                     <p className="text-left text-justify text-md lg:text-lg font-roboto mt-2">If the agent asks to pay 6-year fees during the admission, you must know that the university does not ask for 6 years fees altogether. Students should pay fees on yearly basis.</p>
                 </div>
                 <div className="text-center">
-                    <h2 className="text-md text-red-600 lg:text-xl mt-8 mb-2">
+                    <h2 className="text-md text-red-600 font-semibold lg:text-xl mt-8 mb-2">
                         Fee Structure at VSMU
                     </h2>
                     <p className="text-center text-justify text-md lg:text-lg font-roboto mt-2">The Volgograd State Medical University fees are low and affordable because it is a government-funded university. The Volgograd State Medical University fee structure is given below:</p>
@@ -178,9 +178,9 @@ const Volgograd = () => {
                         <p className="text-red-600 font-semibold text-justify font-roboto mt-4 bg-yellow-300 p-2">RREC team would like to inform you that the exchange rate is extremely volatile and is subjected to change. Candidates are requested to check the current exchange rate and your budget before initiating the admission process.</p>
                     </div>
                 </div>
-                <div className="bg-gray-100 p-6">
+                <div className="bg-gray-100 mt-8">
                     {/* Heading */}
-                    <h2 className="text-2xl font-semibold text-red-600 text-center mb-4">
+                    <h2 className="text-xl font-semibold text-red-600 text-justify mb-4">
                         Faculties at VSMU
                     </h2>
 
@@ -204,46 +204,35 @@ const Volgograd = () => {
                         ))}
                     </div>
                 </div>
+            </div>
 
-
-                {/* Heading */}
-                <h2 className="text-2xl font-semibold text-red-600 text-center mt-6">
-                    Call for more details about the university, admission, processing, and fees at MBBS in VolSMU, Russia:
+            <div className="bg-red-700 mt-10 mb-6 font-roboto rounded-xl px-8 py-10 max-w-4xl lg:max-w-7xl mx-auto text-center font-roboto text-white">
+                <h2 className="text-2xl font-semibold text-white text-center mt-6">
+                    Call for more details about the university, admission, processing, fees at MBBS in VolSMU:
                 </h2>
 
-                 {/* Phone Numbers */}
-                 <div className="mt-4 space-y-2 text-center">
-                    <a href="tel:+917428212236" className="text-gray-700 hover:text-red-600 block">
+                {/* Phone Numbers */}
+                <div className="mt-4 space-y-2 text-center">
+                    <a href="tel:+917042284508" className="text-white hover:text-blue-600 block">
                         📞 +91-7042284508, 7042284509
                     </a>
-                    <a href="tel:18004195827" className="text-gray-700 hover:text-red-600 block">
-                        📞 Toll-free 1800-57-5827 (For Indian Students)
+                    <a href="tel:18005725872" className="text-white hover:text-blue-600 block">
+                        📞 Toll-free 1800-572-5827 (For Indian Students)
                     </a>
-                    <a href="tel:+79515190376" className="text-gray-700 hover:text-red-600 block">
+                    <a href="tel:+79515371133" className="text-white hover:text-blue-600 block">
                         📞 +7-9515371133 (International Students)
                     </a>
                 </div>
 
                 {/* WhatsApp Numbers */}
                 <div className="mt-4 space-y-2 text-center">
-                    <a href="https://wa.me/917428212236" className="text-gray-700 hover:text-red-600 block">
+                    <a href="https://wa.me/917042284508" className="text-white hover:text-blue-600 block">
                         💬 WhatsApp: +91-7042284508
                     </a>
-                    <a href="https://wa.me/79515190376" className="text-gray-700 hover:text-red-600 block">
+                    <a href="https://wa.me/79515371133" className="text-white hover:text-blue-600 block">
                         💬 WhatsApp: +7-9515371133
                     </a>
                 </div>
-            </div>
-
-            <div className="bg-red-700 mt-2 mb-6 rounded-xl px-8 py-16 max-w-4xl lg:max-w-7xl mx-auto text-center font-roboto text-white">
-                <h2 className="text-xl lg:text-2xl mb-4 font-semibold">
-                    Call for more details about the university admission, processing, fees at MBBS in VolSMU, Russia
-                </h2>
-                <h3 className="text-lg lg:text-2xl font-semibold mb-4">
-                    Contact Us: +91-7042284508, 7042284509
-                </h3>
-                <h6 className="mb-4">Toll-free 1800-57-5827 (For Indian Students), +7-9515371133 (International Students)</h6>
-                <h6>Students or parents can write to us at WhatsApp also: +91-7042284508</h6>
             </div>
         </>
     )
